f71c237c87c0f9055d2180c51f8cbe8a6f2dfe9e
Robinss <robinsswei@gmail.com>
2017-01-17 05:23:18 +0000

    different settings for cloud9

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // Load http module.
 var http = require('http');
+var path = require('path');
+
 // Load express module.
 var express = require('express');
 var yaml = require('js-yaml'),
     fs   = require('fs');
 
+var testData = require("./data/mysql.json")
+
 // Initialize app object.
 var app = new express();
+var server = http.createServer(app);
 
 // Let express know there's a public directory.
-app.use(express.static('./public'));
+app.use(express.static(path.resolve(__dirname, 'public')));
  
 // Basic routing.
 app.get('/', function(req, res) {
@@ -19,15 +24,17 @@ app.get('/', function(req, res) {
 });
 
 app.post('/api/listServices', function(req, res) {
-    try {
-      var doc = yaml.safeLoad(fs.readFileSync(__dirname + '/data/asg.yaml', 'utf8'));
-      console.log(doc);
-      res.send(doc)
-    } catch (e) {
-      console.log(e);
-    }
+    // try {
+    //   var doc = yaml.safeLoad(fs.readFileSync(__dirname + '/data/asg.yaml', 'utf8'));
+    //   console.log(doc);
+    //   res.send(doc)
+    // } catch (e) {
+    //   console.log(e);
+    // }
+    res.send(testData)
 });
-// Create server and listen on port 3030.
-http.createServer(app).listen(3000, function() {
-    console.log('Server running...');
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
 });
